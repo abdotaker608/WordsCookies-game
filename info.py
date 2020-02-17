d0f2dee7b5785f2174d75c2b324aca601bbccadd
abdotaker608 <60297982+abdotaker608@users.noreply.github.com>
2020-02-17 13:40:55 +0200

    Add files via upload
    contains the dataset for the game

---
+++ info.py
@@ -0,0 +1,51 @@
+words_database_1 = [
+    'TREE', 'THREE', 'THERE', 'HERE', 'ARE', 'ATE', 'TEA', 'HE', 'HEAR', 'EAR', 'RARE', 'TEAR', 'HEART',
+    'ART', 'HAT', 'RAT', 'RATE', 'HEAT', 'EAT'
+]
+
+words_database_2 = [
+    'MAN', 'WOMAN', 'NOW', 'AN', 'WON', 'OMAN', 'NO', 'OWN'
+]
+
+words_database_3 = [
+    'MALE', 'FEMALE', 'LAME', 'FEEL', 'LEAF', 'MEAL', 'FAME', 'FLEE', 'ME'
+]
+
+levels_map = {
+    1: words_database_1,
+    2: words_database_2,
+    3: words_database_3
+}
+
+letters_count = {
+    'a_count': 0,
+    'b_count': 0,
+    'c_count': 0,
+    'd_count': 0,
+    'e_count': 0,
+    'f_count': 0,
+    'g_count': 0,
+    'h_count': 0,
+    'i_count': 0,
+    'j_count': 0,
+    'k_count': 0,
+    'l_count': 0,
+    'm_count': 0,
+    'n_count': 0,
+    'o_count': 0,
+    'p_count': 0,
+    'q_count': 0,
+    'r_count': 0,
+    's_count': 0,
+    't_count': 0,
+    'u_count': 0,
+    'v_count': 0,
+    'w_count': 0,
+    'x_count': 0,
+    'y_count': 0,
+    'z_count': 0
+}
+
+alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+             ]
